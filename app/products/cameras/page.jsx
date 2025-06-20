f1e5565ea2a76b02e66b3d6ae89bce9314b4de5d
Uttam Kumar <uttammaurya377@gmail.com>
2025-06-20 10:31:23 +0530

    done

--- app/products/cameras/page.jsx
+++ app/products/cameras/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-     <h1> this is phones routes</h1>
+      <h1>This is cameras routes</h1>
     </div>
   )
 }
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Link from 'next/link'
 const page = () => {
   return (
     <div>
       <h1>This is products route</h1>
+      <h2>We offer following products</h2>
+      <ul>
+        <li><Link href={'/products/phones'}>Phones</Link></li>
+        <li><Link href={'/products/laptops'}>Laptops</Link></li>
+        <li><Link href={'/products/cameras'}>Cameras</Link></li>
+      </ul>
     </div>
   )
 }
